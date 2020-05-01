12c22c9c5a804713eda480a6bc434e0af44f8d2a
Louis <louismadrigal26@gmail.com>
2020-05-01 09:19:55 +0800

    fix error in portfolio duplicated icon

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,26 +5,11 @@ class Portfolio extends Component {
     if (this.props.data) {
       var projects = this.props.data.projects.map(function (projects) {
         var projectImage = "images/portfolio/" + projects.image;
-        var projectImage2 = "images/portfolio2/" + projects.image;
         return (
           <div key={projects.title} className="columns portfolio-item">
             <div className="item-wrap">
               <a href={projects.url} title={projects.title}>
                 <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <i className="fa fa-link"></i>
-                </div>
-              </a>
-            </div>
-            <div className="item-wrap">
-              <a href={projects.url} title={projects.title}>
-                <img alt={projects.title} src={projectImage2} />
                 <div className="overlay">
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
